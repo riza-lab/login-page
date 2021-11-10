40b889e284f008b5c8231423998f3d477db57ba7
riza-lab <68800472+riza-lab@users.noreply.github.com>
2021-11-10 11:11:15 +0700

    Add files via upload

---
+++ AbstrakProject/app/src/main/java/com/example/abstrakproject/mainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.abstrakproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
